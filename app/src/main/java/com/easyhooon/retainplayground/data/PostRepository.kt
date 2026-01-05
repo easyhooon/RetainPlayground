58444d162d58cfb55dbf8e3cf203c6df811e7df1
easyhooon <mraz3068@gmail.com>
2026-01-05 22:11:19 +0900

    refactor: repository interface - impl 분리

--- app/src/main/java/com/easyhooon/retainplayground/data/PostRepository.kt
+++ app/src/main/java/com/easyhooon/retainplayground/data/PostRepository.kt
@@ -5,17 +5,25 @@ import com.easyhooon.retainplayground.model.samplePosts
 import dev.zacsweers.metro.Inject
 
 /**
- * Post 데이터를 관리하는 Repository
+ * Post 데이터를 관리하는 Repository Interface
+ */
+interface PostRepository {
+    fun getPosts(): List<Post>
+    fun getPost(postId: Long): Post?
+}
+
+/**
+ * PostRepository 구현체
  * Metro의 @Inject로 의존성 주입
  */
 @Inject
-class PostRepository {
+class DefaultPostRepository : PostRepository {
 
-    fun getPosts(): List<Post> {
+    override fun getPosts(): List<Post> {
         return samplePosts
     }
 
-    fun getPost(postId: Long): Post? {
+    override fun getPost(postId: Long): Post? {
         return samplePosts.find { it.id == postId }
     }
 }
--- app/src/main/java/com/easyhooon/retainplayground/di/AppGraph.kt
+++ app/src/main/java/com/easyhooon/retainplayground/di/AppGraph.kt
@@ -1,19 +1,24 @@
 package com.easyhooon.retainplayground.di
 
 import com.easyhooon.retainplayground.data.PostRepository
+import com.easyhooon.retainplayground.data.DefaultPostRepository
 import com.easyhooon.retainplayground.feature.postdetail.PostDetailPresenter
 import com.easyhooon.retainplayground.feature.postlist.PostListPresenter
+import dev.zacsweers.metro.Binds
 import dev.zacsweers.metro.DependencyGraph
 
 /**
  * Metro DependencyGraph
  * 앱 전체의 의존성 그래프 정의
- * - PostRepository: 데이터 레이어
- * - PostListPresenter, PostDetailPresenter: Composable Presenter
  */
 @DependencyGraph
 interface AppGraph {
-    val postRepository: PostRepository
     val postListPresenter: PostListPresenter
     val postDetailPresenter: PostDetailPresenter
+
+    /**
+     * @Binds: Interface와 구현체 바인딩
+     */
+    @Binds
+    fun bindPostRepository(impl: DefaultPostRepository): PostRepository
 }
